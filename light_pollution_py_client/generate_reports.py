268cc0ac2cbb992be98430f335b8790dfd940df4
Marco Tascini <tascinimarco@tiscali.it>
2025-01-05 20:54:52 +0100

    generate reports py

--- light_pollution_py_client/generate_reports.py
+++ light_pollution_py_client/generate_reports.py
@@ -1,4 +1,6 @@
+import random
 from pandas import read_csv
+import requests
 
 def read_csv_in_list(filepath):
     df_data = read_csv(filepath)
@@ -17,4 +19,25 @@ def read_csv_in_list(filepath):
 
 cities = read_csv_in_list("cities_europe.csv")
 print(cities)
+
+batches = 10
+per_batch = 10
+
+for i in range(batches):
+    data =[]
+    for y in range(per_batch):
+        city_obj = random.choice(cities)
+        severity = random.randint(0,5)
+        data_obj = {
+            "lat":city_obj['lat'],
+            "lon":city_obj['lon'],
+            "city":city_obj['city'],
+            "country":city_obj['country'],
+            "description":"Ligh pollution report at [" + str(city_obj['lat']) +  "," + str(city_obj['lon']) + "]",
+            "severity":severity
+        }
+        data.append(data_obj)
+        
+    response = requests.post('http://localhost:8080/import_reports', json=data)
+    print(response.content)    
 
